3efc67cd636a6109eb0e206f44172f84662bc153
Marcin Tomala <marcin.p.tomala@gmail.com>
2022-01-09 14:08:35 -0500

    remove-row feature completed, closes #3

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const addRow = document.getElementById('add-row');
+const removeRow = document.getElementById('remove-row');
 
 const grid = document.getElementById('grid');
 
 addRow.addEventListener('click', () => {
-    console.log(grid.children[0]);
     for (let i = 0; i < grid.children.length; i++) {
         const newCell = document.createElement("div");
         newCell.className = "item";
         grid.children[i].appendChild(newCell);
     }
-})
+});
+
+removeRow.addEventListener('click', () => {
+    const columns = grid.children;
+    if (columns[0].children.length > 1) {
+        for (let i = 0; i < columns.length; i++) {
+            const columnLength = columns[i].children.length
+            const lastChild = columns[i].children[columnLength-1];
+            columns[i].removeChild(lastChild);
+        }
+    }
+});
